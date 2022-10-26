3528372281be3c79605281c9119f5418cb33ed00
Renze Meinard Mortiga <98935669+renzewithane@users.noreply.github.com>
2022-10-26 23:09:51 +0800

    Update EZLearning2.0.py

--- EZLearning2.0.py
+++ EZLearning2.0.py
@@ -1,4 +1,7 @@
+from distutils.log import Log
+from re import L
 from tkinter import *
+from turtle import back
 from PIL import ImageTk, Image
 import customtkinter
 import tkinter.messagebox
@@ -6,6 +9,7 @@ import tkinter.messagebox
 import pyrebase
 from pygame import FULLSCREEN
 import random
+import shutup;shutup.please()
 
 #SAMPLE EMAIL AND PASSWORD FOR LOGIN:
 #fake@gmail.com
@@ -21,18 +25,19 @@ config = {"apiKey": "AIzaSyBARpuvAyruul-wLV0APfAsd0oT7W5rOuU",
   "measurementId": "G-LF9NKKBDQ8"}
 
 firebase = pyrebase.initialize_app(config)
-auth =firebase.auth()
+auth = firebase.auth()
 
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("green")
 
-class login(customtkinter.CTk):
+class LOGIN(customtkinter.CTk):
     def __init__(self, master):
         self.master = master
         self.master.geometry('900x500')
         self.master.title('EZ-Learning 2.0')
         self.master.iconbitmap("Logo.ico")
         self.master.config(bg="#121212")
+        print("The Login class has been opened")
 
         self._Email = StringVar()
         self._Password = StringVar()
@@ -83,14 +88,14 @@ class login(customtkinter.CTk):
             text="Login",
             border_width=2,
             fg_color=None,
-            command=self.Login)
+            command=self.login)
         self.enter_btn.place(x=270, y=230)
 
         self.signup_btn = customtkinter.CTkButton(self.template,
             text="Create New Account",
             border_width=2,
             fg_color=None,
-            command=self.Signup)
+            command=self.signup)
         self.signup_btn.place(x=85, y=230)
 
         self.app_label = customtkinter.CTkLabel(self.master,
@@ -128,28 +133,28 @@ class login(customtkinter.CTk):
             self.dev_names = Label(self.master, text = 'Dar, John Homer Sayno\nDela Fuente, Ar-Jay\nEchano, Angelo Millares\nMalubay, Arriana Mae Vargas\nMortiga, Renze Meinard', justify=LEFT, font = ('Roboto',7), bg = '#0d9187', fg="#2e2e2e")
             self.dev_names.place(x=10, y=418)
     
-    def Login(self):
+    def login(self):
         if self._Email.get() == '' or self._Password.get() == '': tkinter.messagebox.showinfo('Try Again', 'Please complete the required fields.')
         else: 
             try:
                 login = auth.sign_in_with_email_and_password(self._Email.get(), self._Password.get())
                 print("Login Successfully")
-                home(self.master)
+                NOTES_FOLDER(self.master)
             except:
                 tkinter.messagebox.showinfo('Error', 'Invalid Email or Password. Try again')
 
 
-    def Signup(self):
-        signup(self.master)
+    def signup(self):
+        SIGNUP(self.master)
     
-
-class signup(customtkinter.CTk):
+class SIGNUP(customtkinter.CTk):
     def __init__(self, master):
         self.master = master
         self.master.geometry('900x500')
         self.master.title('EZ-Learning 2.0')
         self.master.iconbitmap("Logo.ico")
         self.master.config(bg="#121212")
+        print("The Signup class has been opened")
 
         self._Username = StringVar()
         self._Email = StringVar()
@@ -223,14 +228,14 @@ class signup(customtkinter.CTk):
             text="Signup Account",
             border_width=2,
             fg_color=None,
-            command=self.Signup)
+            command=self.signup)
         self.enter_btn.place(x=270, y=230)
 
         self.signup_btn = customtkinter.CTkButton(self.template,
             text="Use Existing Account",
             border_width=2,
             fg_color=None,
-            command=self.Login)
+            command=self.login)
         self.signup_btn.place(x=85, y=230)
 
         self.app_label = customtkinter.CTkLabel(self.master,
@@ -268,7 +273,7 @@ class signup(customtkinter.CTk):
             self.dev_names = Label(self.master, text = 'Dar, John Homer Sayno\nDela Fuente, Ar-Jay\nEchano, Angelo Millares\nMalubay, Arriana Mae Vargas\nMortiga, Renze Meinard', justify=LEFT, font = ('Roboto',7), bg = '#0d9187', fg="#2e2e2e")
             self.dev_names.place(x=10, y=418)
     
-    def Signup(self):
+    def signup(self):
         if self._Username.get() == '' or self._Password.get() == '' or self._Email.get() == '' or self._Password.get() == '' or self._Confirm_Password.get() == '': 
             tkinter.messagebox.showinfo('Try Again', 'Please input data')
         else:
@@ -280,40 +285,164 @@ class signup(customtkinter.CTk):
                 except:
                     tkinter.messagebox.showinfo('Invalid', 'Email Exist, please input another account')
 
-    def Login(self):
-        login(self.master)
+    def login(self):
+        LOGIN(self.master)
 
-class home(customtkinter.CTk):
+class NOTES_FOLDER(customtkinter.CTk):
     def __init__(self, master):
         self.master = master
+        self.threelinemenu_dark = ImageTk.PhotoImage(Image.open("3line_dark.png"))
+        self.threelinemenu_light = ImageTk.PhotoImage(Image.open("3line_light.png"))
+        self.closemenu_dark = ImageTk.PhotoImage(Image.open("close_dark.png"))
+        self.closemenu_light = ImageTk.PhotoImage(Image.open("close_light.png"))
+        self.backframe()
+        self.side_menu_icon()    
+        print("The Home class has been opened")
 
-        def backframe():
-            defaultbackground = Frame(self.master,
-            width=900,
-            height=500,
-            background=self.master.cget("bg"))
-            defaultbackground.place(x=0, y=0)
+    def backframe(self):
+        Frame(self.master,
+        width=900,
+        height=500,
+        background=self.master.cget("bg")).place(x=0, y=0)
 
-        def default_home():
-            backframe()
-        
-        default_home()
+    def side_menu_icon(self):
+        if self.master.cget("bg") == "#121212":
+            Button(self.master,
+            image=self.threelinemenu_dark,
+            command=self.side_menu,
+            border=0,
+            bg=self.master.cget("bg"),
+            activebackground=self.master.cget("bg")).place(x=5,y=8)
+        elif self.master.cget("bg") == "#0d9187":
+            Button(self.master,
+            image= self.threelinemenu_light,
+            command=self.side_menu,
+            border=0,
+            bg=self.master.cget("bg"),
+            activebackground=self.master.cget("bg")).place(x=5,y=8)
+    
+    def side_menu(self):
+        THREELINE_MENU(self.master)
 
-        
+class QUIZ_FOLDER(customtkinter.CTk):
+    ""
+
+class RECYCLE_BIN(customtkinter.CTk):
+    ""
+
+class PROFILE_SETTINGS(customtkinter.CTk):
+    ""
+
+class THREELINE_MENU(customtkinter.CTk):
+    def __init__(self, master):
+        self.master = master
+        self.closemenu_dark = ImageTk.PhotoImage(Image.open("close_dark.png"))
+        self.closemenu_light = ImageTk.PhotoImage(Image.open("close_light.png"))
+
+        self.burger_menu()
+        print("The Three-Line Menu class has been opened")
+
+    def menu(self):
+        pass
+    
+    def burger_menu(self):
+        fg_color = "#ffffff"
+        activefg = "#ffffff"
+        fontstyle = ("Roboto", 17, "bold")
+
+        if self.master.cget("bg") == "#121212":
+            bg_color = "#3B3838"
+            activebg = "#6B6565"
+        else: 
+            bg_color = "#15559B"
+            activebg = "#114681"
+            
+        threeline_menu = Frame(self.master,
+        width=286,
+        height=500,
+        background=bg_color)
+        threeline_menu.place(x=0, y=0)
+        if bg_color == "#3B3838":
+            close_button = self.closemenu_dark
+        else: close_button = self.closemenu_light
+        Button(self.master,
+        image=close_button,
+        command=self.menu,
+        border=0,
+        bg=bg_color,
+        activebackground=bg_color).place(x=7,y=10)
+        Notes = Button(self.master,
+                text="     Notes",
+                command=self.menu,
+                anchor=W,
+                font=fontstyle,
+                border=0,
+                fg=fg_color,
+                activeforeground=activefg,
+                bg=bg_color,
+                activebackground=activebg,
+                width=20)
+        Quizzes = Button(self.master,
+                text="     Quizzes",
+                anchor=W,
+                font=fontstyle,
+                command=self.menu,
+                border=0,
+                fg=fg_color,
+                activeforeground=activefg,
+                bg=bg_color,
+                activebackground=activebg,
+                width=20)
+        Recycle_Bin = Button(self.master,
+                text="     Recycle Bin",
+                anchor=W,
+                font=fontstyle,
+                command=self.menu,
+                border=0,
+                fg=fg_color,
+                activeforeground=activefg,
+                bg=bg_color,
+                activebackground=activebg,
+                width=20)
+        Profile_Settings = Button(self.master,
+                text="     Profile Settings",
+                anchor=W,
+                font=fontstyle,
+                command=self.menu,
+                border=0,
+                fg=fg_color,
+                activeforeground=activefg,
+                bg=bg_color,
+                activebackground=activebg,
+                width=20)
+        Logout = Button(self.master,
+                text="     Logout",
+                anchor=W,
+                font=fontstyle,
+                command=self.menu,
+                border=0,
+                fg=fg_color,
+                activeforeground=activefg,
+                bg=bg_color,
+                activebackground=activebg,
+                width=20)
+        Notes.place(x=0,y=75)
+        Quizzes.place(x=0, y=123)
+        Recycle_Bin.place(x=0, y=171)
+        Profile_Settings.place(x=0, y=219)
+        Logout.place(x=0, y=267)
+    
 window = Tk()
 window.resizable(False, False)
-ws = window.winfo_screenwidth() # width of the screen
-hs = window.winfo_screenheight() # height of the screen
-w = 900 # width for the Tk root
-h = 500 # height for the Tk root
+ws = window.winfo_screenwidth()
+hs = window.winfo_screenheight()
+w = 900
+h = 500
 
-# calculate x and y coordinates for the Tk root window
 x = (ws/2) - (w/2)
 y = (hs/2) - (h/1.7)
 
-# set the dimensions of the screen 
-# and where it is placed
 window.geometry('%dx%d+%d+%d' % (w, h, x, y))
 if __name__ == "__main__":
-    the_hundreds = signup(window)
+    the_hundreds = SIGNUP(window)
     window.mainloop()
