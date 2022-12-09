140a32badea772cc12f51865d3694f7bd5f5c8f5
Angelo Echano <grey12angelo@gmail.com>
2022-12-09 14:12:52 +0800

    added share feature

---
+++ model/Shared.py
@@ -0,0 +1,8 @@
+class Share:
+  def __init__(self, shareId, folderId, sharedBy, sharedTo, status):
+
+    self.shareId = shareId
+    self.folderId = folderId
+    self.sharedBy = sharedBy
+    self.sharedTo = sharedTo
+    self.status = status
